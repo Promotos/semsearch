fb0b08789eed6dad39ea4a4e12b5bf05b6db3f41
Matthias Lachmann <malachmann@deloitte.com>
2023-12-02 22:47:15 -0600

    add indexing

--- crawler/src/crawler_app.py
+++ crawler/src/crawler_app.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 from os import environ
-import chromadb
+import hashlib
+from chromadb import Collection, PersistentClient
+from chromadb.utils import embedding_functions
+from pypdf import PdfReader
+
+sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 
 def get_env(name:str) -> str:
     val = environ.get(name)
@@ -11,6 +16,10 @@ def get_env(name:str) -> str:
 INDEX_ROOT = get_env('SEMSEARCH_INDEX_ROOT')
 DB_PATH = get_env('SEMSEARCH_DB')
 
+def filehash(file:str) -> str:
+    with open(file, 'rb', buffering=0) as f:
+        return hashlib.file_digest(f, 'sha256').hexdigest()
+
 def get_files_to_index(root:str) -> [str]:
     result = []
     for path in Path(root).rglob('*'):
@@ -18,12 +27,37 @@ def get_files_to_index(root:str) -> [str]:
             result.append(path)
     return result
 
+def index_file(file_col:Collection, file:str, hash:str):
+    print(f"Index file {file} ... ", end="")
+    reader = PdfReader(file)
+    for page in reader.pages:
+        text = page.extract_text()
+        file_col.add(
+            documents=[text],
+            metadatas=[{'hash': hash, 'file': str(file)}],
+            ids=[f"{hash}-{page.page_number}"]
+        )
+    print("ok")
+
+def index_missing(file_col:Collection, files:[str]):
+    for file in files:
+        hash = filehash(file)
+        res = file_col.get(where={'hash': {'$eq': hash}})
+        if len(res['ids']) == 0:
+            index_file(file_col, file=file, hash=hash)
+
 print(f"Load database from '{DB_PATH}' ... ", end="")
-client = chromadb.PersistentClient(path=DB_PATH)
+client = PersistentClient(path=DB_PATH)
 print("ok")
 
 print(f"List files to index in {INDEX_ROOT} ... ", end="")
 files = get_files_to_index(INDEX_ROOT)
 print(f"found {len(files)} files")
 
+file_collection = client.get_or_create_collection(name="files", embedding_function=sentence_transformer_ef)
+print(f"{file_collection.count()} indexed elements")
 
+index_missing(file_collection, files)
+
+#res = file_collection.query(query_texts=['Who is Dietmar Spangl?'])
+#print(res)
